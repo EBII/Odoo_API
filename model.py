28acb428a0f2e7bd8a1cc7111415894e469acd85
Eric BOUHANA <eric@ebii.fr>
2016-06-08 09:35:18 +0200

    lets start new db

--- model.py
+++ model.py
@@ -5,20 +5,20 @@ from openerp import models, api, fields, _
 
 class ApiTerrain(models.Model):
     _name = 'apihelper.apiterrain'
+    _rec_name = 'emplacement'
 
     # https://www.odoo.com/documentation/8.0/reference/orm.html#fields
-    emplacement = fields.Char('Emplacement')
-    ressource    = fields.Many2many ('apihelper.apiressource','terrain_ressource','apiTerrain','apiRessource','Ressource')
-    proprietaire= fields.Many2one('res.partner',u'Propriétaire')
-    apiculteur  = fields.Many2one('res.partner','Apiculteur') #plusieurs apiculteur/ terrain
-    traitement  = fields.One2many ('apihelper.apitraitement', 'terrain')
-    actif       = fields.Boolean('actif')
+    emplacement  = fields.Char('Emplacement')
+    ressource    = fields.Many2many('apihelper.apiressource') #,'terrain_ressource','apiTerrain','apiRessource','Ressource'
+    proprietaire = fields.Many2one('res.partner',u'Propriétaire')
+    apiculteur   = fields.Many2one('res.partner','Apiculteur') #plusieurs apiculteur/ terrain
+    traitement   = fields.One2many('apihelper.apitraitement', 'terrain')
+    actif        = fields.Boolean('actif')
 
-    def view_terrain(self):
-        return ApiTerrain.get_default()
 
 class ApiTraitement(models.Model):
     _name = 'apihelper.apitraitement'
+    _rec_name = 'intitule'
 
     #liste des traitements réaliser sur le rucher par agriculteur ou par l'apiculteur
     intitule = fields.Char('traitement')
@@ -35,11 +35,16 @@ class ApiRessource(models.Model):
     periode_start = fields.Date('debut floraison')
     periode_stop  = fields.Date('fin floraison')
     annee         = fields.Integer() #ne mettre que l'année
-    position      =fields.Integer()  # champ compute fleuri après et avant une autre ressource
+    position      = fields.Integer()  # champ compute fleuri après et avant une autre ressource
+#display_name = fields.Char(compute)
+
+    #creer la fonction qui agrege le name
+
 
 class ApiRucher(models.Model):
     #Rucher installe
     _name = 'apihelper.apirucher'
 
+    name    = fields.Char ('Nom du rucher')
     terrain =fields.Many2one('apihelper.apiterrain','Emplacement')
-    nombre = fields.Integer('Nombre de Ruche')
+    nombre  = fields.Integer('Nombre de Ruche')
