0a7efe877b451d480ce5824466fc315c0a8e19ef
MonsieurB <eric@ebii.fr>
2016-06-22 16:07:01 +0200

    wizard avec les boutons et les methodes to be continued

--- wizard/apihelper_wizard_model.py
+++ wizard/apihelper_wizard_model.py
@@ -8,8 +8,11 @@ class WizardApihelper(models.TransientModel):
     _name = 'wizard.apihelper'
     _description = u"wizard de realisation d'action sur les terrains pour l'Api Odoo"
 
+    unId        = fields.Selection([('none','none'),(1,1),(2,2),(3,3),(4,4),(5,5)],'Un id de model', default='none')
     information = fields.Char('texte')
-    choix_model = fields.Many2one('apihelper.apiterrain')
+    choix_model = fields.Selection([('apihelper.ressource','Les Ressources'),('apihelper.apiterrain','Les Terrains')
+                                   ,('apihelper.apitraitement','Les Traitements'),('apihelper.apifloraison','Les Floraisons')
+                                   ,('apihelper.apirucher','Les Ruchers')], "Le modele",  required=True)
 
     @api.multi
     def get_wizard(self,*args):
@@ -28,6 +31,32 @@ class WizardApihelper(models.TransientModel):
                         }
 
         raise UserError("Voici le wizard qui se lance pas!")
+    @api.multi
+    def wz_Browse(self):
+        for me in self:
+            un_id     = me.unId
+            un_model  = me.choix_model
+            une_value = me.information
+            if un_id != 'none':
+                modelId = un_model+'_id'
+               # rs = self.env[un_model].browse(un_id)
+
+                return {'type': 'ir.actions.act_window',
+                'res_model': un_model,
+                'name' : 'Read',
+                'view_mode': 'form',
+                'nodestroy': True,
+                'target': 'curent',
+                'domain': [(modelId,'=',un_id)],
+                #'context': {'read': True}
+                }
 
 
+
+    @api.multi
+    def wz_Search(self):
+        for me in self:
+            un_model = me.choix_model
+            une_value = me.information
+            raise UserError("le model : "+str(un_model) +"  le texte : "+une_value )
 
